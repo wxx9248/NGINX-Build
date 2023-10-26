d721b251a4b98c74f31c50c3e57e75c25345d8d7
wxx9248 <wxx9248@wxx9248.top>
2023-10-25 20:27:48 -0400

    Add Architecture Variant Support

--- script/linux/util/tag.py
+++ script/linux/util/tag.py
@@ -1,43 +1,60 @@
 # -*- coding: utf-8 -*-
 import typing
 
-ArchTagType: typing.TypeAlias = typing.Optional[str]
-DistroTagType: typing.TypeAlias = str
+# Distro(distro, version)
+DistroType: typing.TypeAlias = typing.Tuple[str, typing.Optional[str]]
+# Arch(arch, variant)
+ArchType: typing.TypeAlias = typing.Tuple[typing.Optional[str], typing.Optional[str]]
 
-ARCH_DICT: typing.Dict[str, ArchTagType] = {
-    "amd64": None,
-    "aarch64": "arm64v8"
+DISTRO_DICT: typing.Dict[str, DistroType] = {
+    "ubuntu/jammy": ("ubuntu", "jammy"),
+    "archlinux": ("archlinux", None),
+    "alpine": ("alpine", None)
 }
 
-DISTRO_DICT: typing.Dict[str, DistroTagType] = {
-    "focal": "ubuntu:focal",
-    "jammy": "ubuntu:jammy",
-    "archlinux": "archlinux",
-    "alpine": "alpine"
+ARCH_DICT: typing.Dict[str, ArchType] = {
+    "amd64": ("amd64", None),
+    "arm64/v8": ("arm64", "v8"),
+    "arm32/v6": ("arm32", "v6"),
+    "arm32/v7": ("arm32", "v7"),
+    "i386": ("i386", None),
+    "ppc64le": ("ppc64le", None),
+    "s390x": ("s390x", None)
 }
 
-TAG_OVERRIDE_DICT: typing.Dict[typing.Tuple[str, str], str] = {
-    ("archlinux", "aarch64"): "agners/archlinuxarm-arm64v8"
+OVERRIDE_DICT: typing.Dict[typing.Tuple[DistroType, ArchType], str] = {
+    (("archlinux", None), ("arm64", "v8")): "agners/archlinuxarm-arm64v8"
 }
 
 
-def get_docker_image_tag(distro: str, arch: str) -> str:
-    if distro not in DISTRO_DICT:
-        raise NotImplementedError(f"Unsupported distro: {distro}")
+def get_docker_image_tag(distro_version: str, arch_variant: str) -> str:
+    if distro_version not in DISTRO_DICT:
+        raise NotImplementedError(f"Unsupported distro version: {distro_version}")
 
-    if arch not in ARCH_DICT:
-        raise NotImplementedError(f"Unsupported arch: {arch}")
+    if arch_variant not in ARCH_DICT:
+        raise NotImplementedError(f"Unsupported arch variant: {arch_variant}")
 
-    if (distro, arch) in TAG_OVERRIDE_DICT:
-        return TAG_OVERRIDE_DICT[(distro, arch)]
+    distro = DISTRO_DICT.get(distro_version)
+    arch = ARCH_DICT.get(arch_variant)
 
-    tag = ""
+    if (distro, arch) in OVERRIDE_DICT:
+        return OVERRIDE_DICT[(distro, arch)]
 
-    arch_tag: ArchTagType = ARCH_DICT.get(arch)
-    if arch_tag is not None:
-        tag = arch_tag + "/"
+    tag_builder = []
 
-    distro_tag: DistroTagType = DISTRO_DICT.get(distro)
-    tag += distro_tag
+    if arch[0] is not None:
+        tag_builder.append(arch[0])
 
-    return tag
+    if arch[1] is not None:
+        tag_builder.append(arch[1])
+
+    if len(tag_builder) != 0:
+        tag_builder.append("/")
+
+    tag_builder.append(distro[0])
+
+    if distro[1] is not None:
+        tag_builder.append(":")
+        tag_builder.append(distro[1])
+
+    return "".join(tag_builder)
