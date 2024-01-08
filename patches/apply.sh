b3510ed8ffb8b1777254fde8774439508746958a
wxx9248 <wxx9248@wxx9248.top>
2024-01-07 21:02:40 -0500

    Fix njs build

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,2 +1,5 @@
 # LibreSSL OpenBSD codebase branch fix
 echo libressl-v3.8.2 >workspace/libressl/OPENBSD_BRANCH
+
+# https://github.com/nginx/njs/issues/684
+patch workspace/njs/external/njs_xml_module.c patches/njs.issue.684.patch
